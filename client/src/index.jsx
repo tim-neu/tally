f93807cc790f2d9adb44d202fe330ac743b18514
Tim <timotheeaneumann@gmail.com>
2016-12-30 18:24:14 -0800

    Setup tests for voting
    hot reloading is down

---
+++ client/src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Voting from './components/Voting';
+
+const pair = ['Trainspotting', '28 Days Later'];
+
+ReactDOM.render(
+  <Voting pair={pair} />,
+  document.getElementById('app')
+);
+
+console.log('I am alive!');
